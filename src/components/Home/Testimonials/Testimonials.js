7c075ec3d9123dc2f73683ef8031eead378ed8ea
Zach Tapia <zachtapia@gmail.com>
2020-07-15 11:08:50 -0700

    completed properties page

--- src/components/Home/Testimonials/Testimonials.js
+++ src/components/Home/Testimonials/Testimonials.js
@@ -8,8 +8,8 @@ const Testimonials = () => {
     background: "#888",
     width: "3vw",
     height: "3vw",
-    maxWidth: "40px",
-    maxHeight: "40px",
+    maxWidth: "25px",
+    maxHeight: "25px",
     display: "inline-block",
     margin: "0 12px",
   }
@@ -30,35 +30,54 @@ const Testimonials = () => {
         >
           <div className={styles.testimonials__card}>
             <div className={styles.testimonials__card__quote}>
-              " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. "
+              " Working with Christine was easy. We accepted an offer within 1
+              week of listing our home. She always answers calls and emails
+              promptLy. Her knowledge the area really helped in us determining
+              our asking price. Would highly recommend her for your realtor
+              needs. "
             </div>
             <div className={styles.testimonials__card__author}>
-              - Buster Tapia
+              - Mark & Debbie Sanchez
             </div>
           </div>
           <div className={styles.testimonials__card}>
             <div className={styles.testimonials__card__quote}>
-              " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. "
+              " Christine did an incredible job both listing and selling our old
+              home and finding and guiding me and my wife on the purchase of our
+              new home. This was particularly impressive as the timing of the
+              market was key to ensuring I sold my old home for top dollar. She
+              guided the staging and listing of our old home to ensure a top
+              selling price, while negotiating with the seller of our new home
+              all under an extreme time constraint to ensure wouldn't lose the
+              bid on our dream home. Christine worked a miracle! If she did it
+              for us, she can do it for you. Awesome job Christine!! "
             </div>
             <div className={styles.testimonials__card__author}>
-              - Buster Tapia
+              - Xavier Sanchez
             </div>
           </div>
           <div className={styles.testimonials__card}>
             <div className={styles.testimonials__card__quote}>
-              " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. "
+              " This is our 3rd property with Christine. We own in L.A. County,
+              San Bernardino County and Riverside County and Christine has
+              proven to know the market in all areas, we even used Christine to
+              find tenants for our rental and it took less than a week.
+              Christine is a great communicator and negotiator, we Highly
+              recommend Christine and will use her again on our next property. "
             </div>
             <div className={styles.testimonials__card__author}>
-              - Buster Tapia
+              - Ronnie Delgado
+            </div>
+          </div>
+          <div className={styles.testimonials__card}>
+            <div className={styles.testimonials__card__quote}>
+              " Christine was very professional and helpful. Great personality,
+              trustworthy, on time for meetings and getting paperwork done. She
+              knew exactly what we needed and wanted. We were very happy from
+              start to finish. "
+            </div>
+            <div className={styles.testimonials__card__author}>
+              - Lisa Anne Taormina
             </div>
           </div>
         </Carousel>
--- src/components/Properties/PropertiesList/PropertiesList.js
+++ src/components/Properties/PropertiesList/PropertiesList.js
@@ -19,32 +19,124 @@ const PropertiesList = () => {
 
   const allProperties = [
     {
-      address: "1 Easy St.",
-      image: "house1.jpg",
+      address: "15238 Tricia Ln.",
+      city: "La Mirada",
+      image: "TriciaLn.png",
       sold: true,
-      dateSold: "March 15, 2020",
-      price: "$1,000,000",
+      price: "$525,000",
+      bedBath: "4 Bed / 2 Bath",
     },
     {
-      address: "12 Easy St.",
-      image: "house2.jpg",
+      address: "11941 Groveside Ave.",
+      city: "Whittier",
+      image: "GrovesideAve.png",
       sold: true,
-      dateSold: "March 15, 2020",
-      price: "$1,000,000",
+      price: "$620,000",
+      bedBath: "3 Bed / 2 Bath",
     },
     {
-      address: "123 Easy St.",
-      image: "house3.jpg",
+      address: "5310 E Big Sky Ln.",
+      city: "Anaheim Hills",
+      image: "BigSkyLn.png",
       sold: true,
-      dateSold: "March 15, 2020",
-      price: "$1,000,000",
+      price: "$845,000",
+      bedBath: "4 Bed / 2 Bath",
     },
     {
-      address: "1234 Easy St.",
-      image: "house4.jpg",
+      address: "11665 Faculty Dr.",
+      city: "Norwalk",
+      image: "FacultyDr.png",
       sold: true,
-      dateSold: "March 15, 2020",
-      price: "$1,000,000",
+      price: "$575,000",
+      bedBath: "3 Bed / 1 Bath",
+    },
+    {
+      address: "15 Villa Milano",
+      city: "Lake Elsinore",
+      image: "VillaMilano.png",
+      sold: true,
+      price: "$405,000",
+      bedBath: "4 Bed / 2 Bath",
+    },
+    {
+      address: "12041 Parise Dr.",
+      city: "Whittier",
+      image: "PariseDr.png",
+      sold: true,
+      price: "$453,000",
+      bedBath: "3 Bed / 2 Bath",
+    },
+    {
+      address: "1861 San Jose Ave.",
+      city: "La Habra",
+      image: "SanJoseAve.png",
+      sold: true,
+      price: "$665,000",
+      bedBath: "3 Bed / 2 Bath",
+    },
+    {
+      address: "93 Carriage Way #147",
+      city: "Pomona",
+      image: "CarriageWay.png",
+      sold: true,
+      price: "$305,000",
+      bedBath: "2 Bed / 2 Bath",
+    },
+    {
+      address: "9588 Mountain Pine Ct.",
+      city: "Phelan",
+      image: "MountainPineCt.png",
+      sold: true,
+      price: "$265,000",
+      bedBath: "3 Bed / 2 Bath",
+    },
+    {
+      address: "10251 Belcher St.",
+      city: "Downey",
+      image: "BelcherSt.png",
+      sold: true,
+      price: "$400,000",
+      bedBath: "4 Bed / 2 Bath",
+    },
+    {
+      address: "1215 N San Gabriel Ave. #309",
+      city: "Azusa",
+      image: "SanGabrielAve.png",
+      sold: true,
+      price: "$330,000",
+      bedBath: "3 Bed / 3 Bath",
+    },
+    {
+      address: "4118 Warrington Ave.",
+      city: "Pico Rivera",
+      image: "WarringtonAve.png",
+      sold: true,
+      price: "$420,000",
+      bedBath: "3 Bed / 3 Bath",
+    },
+    {
+      address: "21210 E Arrow Hwy. #151",
+      city: "Covina",
+      image: "ArrowHwy.png",
+      sold: true,
+      price: "$50,000",
+      bedBath: "2 Bed / 1 Bath",
+    },
+    {
+      address: "1 Calavera Rd.",
+      city: "Pinon Hills",
+      image: "CalaveraRd.png",
+      sold: true,
+      price: "$15,000",
+      bedBath: "2.4 Acres",
+    },
+    {
+      address: "17376 Woodentree Ln.",
+      city: "Riverside",
+      image: "WoodenTreeLn.png",
+      sold: true,
+      price: "$440,000",
+      bedBath: "4 Bed / 3 Bath",
     },
   ]
 
@@ -61,6 +153,8 @@ const PropertiesList = () => {
         <Property
           key={property.address}
           address={property.address}
+          city={property.city}
+          bedBath={property.bedBath}
           sold={property.sold}
           dateSold={property.dateSold}
           price={property.price}
@@ -70,12 +164,7 @@ const PropertiesList = () => {
     })
   }
 
-  return (
-    <div className={styles.propertiesList}>
-      <h1>Test</h1>
-      {renderList()}
-    </div>
-  )
+  return <div className={styles.propertiesList}>{renderList()}</div>
 }
 
 export default PropertiesList
