8b3ba402b90c4a39d2da2354b51764c7821b3d49
Zach Tapia <zachtapia@gmail.com>
2020-07-19 14:23:26 -0700

    created contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
 import ContactHeader from "../components/Contact/ContactHeader/ContactHeader"
+import ContactForm from "../components/Contact/ContactForm/ContactForm"
 
 const contact = () => {
   return (
     <Layout>
       <ContactHeader />
+      <ContactForm />
     </Layout>
   )
 }
